65e9f37f869a454e10127984affc7709514728cf
Bifortis <bifortis@Bifortiss-Mac-mini-3.local>
2015-03-18 10:29:35 +0530

    Initial Commit

---
+++ EasyPost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyPost
+//
+//  Created by Bifortis on 3/18/15.
+//  Copyright (c) 2015 Narek Gevorgyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasyPost/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyPost
+//
+//  Created by Bifortis on 3/18/15.
+//  Copyright (c) 2015 Narek Gevorgyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
